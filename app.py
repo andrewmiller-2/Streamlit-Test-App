c6495ab9d5567dbba3920034b18af857ef139572
andrewmiller-2 <andrew.miller2@student.fairfield.edu>
2025-11-12 17:58:04 -0500

    v.02

--- app.py
+++ app.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 st.title("Test App")
 st.write("Welcome to your first Streamlit app.")
+
 name = st.text_input("Hey what's your name?")
+
+if name:
+    st.success(f"Hello, {name}, welcome to the app.")
